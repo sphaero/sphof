e6a39d572a07b891977d94ecbfa48a7fc310e513
Arnaud Loonstra <arnaud@sphaero.org>
2015-06-20 17:14:45 +0200

    still working around readthedocs issues

--- docs/conf.py
+++ docs/conf.py
@@ -268,5 +268,5 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return Mock()
 
-MOCK_MODULES = ['ZOCP', 'PIL', 'tkinter', 'ImageDraw', 'ImageTk']
-sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+#MOCK_MODULES = ['ZOCP', 'PIL', 'tkinter', 'ImageDraw', 'ImageTk']
+#sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
