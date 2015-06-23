faf645db2e200e37277597e5f9cd2a7a437d8869
Arnaud Loonstra <arnaud@sphaero.org>
2015-06-23 13:03:43 +0000

    forks to chopsticks

--- dining_philosopher.py
+++ dining_philosopher.py
@@ -8,8 +8,8 @@ from sphof import PhilosopherActor, LeadActor
 class MyPhilosopher(PhilosopherActor):
     
     def setup(self):
-        self.register_string("fork1", "", "rs")
-        self.register_string("fork2", "", "rs")
+        self.register_string("chopstick1", "", "rs")
+        self.register_string("chopstick2", "", "rs")
         self.register_string("state", "HUNGRY", "re")
 
     def update(self):
@@ -31,8 +31,8 @@ class MyPhilosopher(PhilosopherActor):
 
     def on_peer_signaled(self, peer, name, data):
         if name == "Waiter":
-            if self.get_value("fork1") == self.name() and\
-                self.get_value("fork2") == self.name():
+            if self.get_value("chopstick1") == self.name() and\
+                self.get_value("chopstick2") == self.name():
                     print("{0}:Jay food, eating....{1}".format(self.name(), len(self.topics)))
                     if self.get_value("state") != "EATING":
                         self.emit_signal("state", "EATING")
@@ -49,11 +49,11 @@ class MyPhilosopher(PhilosopherActor):
 class Waiter(LeadActor):
 
     def setup(self):
-        self.register_string("fork1", "", "rwe")
-        self.register_string("fork2", "", "rwe")
-        self.register_string("fork3", "", "rwe")
-        self.register_string("fork4", "", "rwe")
-        self.register_string("fork5", "", "rwe")
+        self.register_string("chopstick1", "", "rwe")
+        self.register_string("chopstick2", "", "rwe")
+        self.register_string("chopstick3", "", "rwe")
+        self.register_string("chopstick4", "", "rwe")
+        self.register_string("chopstick5", "", "rwe")
         self.register_string("state1", "", "rws")
         self.register_string("state2", "", "rws")
         self.register_string("state3", "", "rws")
@@ -68,86 +68,86 @@ class Waiter(LeadActor):
     def on_peer_enter(self, peer, name, *args, **kwargs):
         if name == "Descartes":
             self.signal_subscribe(self.uuid(), "state1", peer, "state")
-            self.signal_subscribe(peer, "fork1", self.uuid(), "fork1")
-            self.signal_subscribe(peer, "fork2", self.uuid(), "fork2")
+            self.signal_subscribe(peer, "chopstick1", self.uuid(), "chopstick1")
+            self.signal_subscribe(peer, "chopstick2", self.uuid(), "chopstick2")
         if name == "Plato":
             self.signal_subscribe(self.uuid(), "state2", peer, "state")
-            self.signal_subscribe(peer, "fork1", self.uuid(), "fork2")
-            self.signal_subscribe(peer, "fork2", self.uuid(), "fork3")
+            self.signal_subscribe(peer, "chopstick1", self.uuid(), "chopstick2")
+            self.signal_subscribe(peer, "chopstick2", self.uuid(), "chopstick3")
         if name == "Aristotle":
             self.signal_subscribe(self.uuid(), "state3", peer, "state")
-            self.signal_subscribe(peer, "fork1", self.uuid(), "fork3")
-            self.signal_subscribe(peer, "fork2", self.uuid(), "fork4")
+            self.signal_subscribe(peer, "chopstick1", self.uuid(), "chopstick3")
+            self.signal_subscribe(peer, "chopstick2", self.uuid(), "chopstick4")
         if name == "Socrates":
             self.signal_subscribe(self.uuid(), "state4", peer, "state")
-            self.signal_subscribe(peer, "fork1", self.uuid(), "fork4")
-            self.signal_subscribe(peer, "fork2", self.uuid(), "fork5")
+            self.signal_subscribe(peer, "chopstick1", self.uuid(), "chopstick4")
+            self.signal_subscribe(peer, "chopstick2", self.uuid(), "chopstick5")
         if name == "Kant":
             self.signal_subscribe(self.uuid(), "state5", peer, "state")
-            self.signal_subscribe(peer, "fork1", self.uuid(), "fork5")
-            self.signal_subscribe(peer, "fork2", self.uuid(), "fork1")
+            self.signal_subscribe(peer, "chopstick1", self.uuid(), "chopstick5")
+            self.signal_subscribe(peer, "chopstick2", self.uuid(), "chopstick1")
     
     def on_peer_signaled(self, peer, name, data):
         if name == "Descartes":
             if data[1] == "HUNGRY":
-                if not self.get_value("fork1") and not self.get_value("fork2"):
-                    # forks are available
-                    self.emit_signal("fork1", "Descartes")
-                    self.emit_signal("fork2", "Descartes")
+                if not self.get_value("chopstick1") and not self.get_value("chopstick2"):
+                    # chopsticks are available
+                    self.emit_signal("chopstick1", "Descartes")
+                    self.emit_signal("chopstick2", "Descartes")
             if data[1] == "THINKING":
-                if self.get_value("fork1") == "Descartes" and\
-                        self.get_value("fork1") == "Descartes":
-                    # forks are released
-                    self.emit_signal("fork1", "")
-                    self.emit_signal("fork2", "")
+                if self.get_value("chopstick1") == "Descartes" and\
+                        self.get_value("chopstick1") == "Descartes":
+                    # chopsticks are released
+                    self.emit_signal("chopstick1", "")
+                    self.emit_signal("chopstick2", "")
         if name == "Plato":
             if data[1] == "HUNGRY":
-                if not self.get_value("fork2") and not self.get_value("fork3"):
-                    # forks are available
-                    self.emit_signal("fork2", "Plato")
-                    self.emit_signal("fork3", "Plato")
+                if not self.get_value("chopstick2") and not self.get_value("chopstick3"):
+                    # chopsticks are available
+                    self.emit_signal("chopstick2", "Plato")
+                    self.emit_signal("chopstick3", "Plato")
             if data[1] == "THINKING":
-                if self.get_value("fork2") == "Plato" and\
-                        self.get_value("fork3") == "Plato":
-                    # forks are released
-                    self.emit_signal("fork2", "")
-                    self.emit_signal("fork3", "")       
+                if self.get_value("chopstick2") == "Plato" and\
+                        self.get_value("chopstick3") == "Plato":
+                    # chopsticks are released
+                    self.emit_signal("chopstick2", "")
+                    self.emit_signal("chopstick3", "")       
         if name == "Aristotle":
             if data[1] == "HUNGRY":
-                if not self.get_value("fork3") and not self.get_value("fork4"):
-                    # forks are available
-                    self.emit_signal("fork3", "Aristotle")
-                    self.emit_signal("fork4", "Aristotle")
+                if not self.get_value("chopstick3") and not self.get_value("chopstick4"):
+                    # chopsticks are available
+                    self.emit_signal("chopstick3", "Aristotle")
+                    self.emit_signal("chopstick4", "Aristotle")
             if data[1] == "THINKING":
-                if self.get_value("fork3") == "Aristotle" and\
-                        self.get_value("fork4") == "Aristotle":
-                    # forks are released
-                    self.emit_signal("fork3", "")
-                    self.emit_signal("fork4", "")
+                if self.get_value("chopstick3") == "Aristotle" and\
+                        self.get_value("chopstick4") == "Aristotle":
+                    # chopsticks are released
+                    self.emit_signal("chopstick3", "")
+                    self.emit_signal("chopstick4", "")
         if name == "Socrates":
             if data[1] == "HUNGRY":
-                if not self.get_value("fork4") and not self.get_value("fork5"):
-                    # forks are available
-                    self.emit_signal("fork4", "Socrates")
-                    self.emit_signal("fork5", "Socrates")
+                if not self.get_value("chopstick4") and not self.get_value("chopstick5"):
+                    # chopsticks are available
+                    self.emit_signal("chopstick4", "Socrates")
+                    self.emit_signal("chopstick5", "Socrates")
             if data[1] == "THINKING":
-                if self.get_value("fork4") == "Socrates" and\
-                        self.get_value("fork5") == "Socrates":
-                    # forks are released
-                    self.emit_signal("fork4", "")
-                    self.emit_signal("fork5", "")
+                if self.get_value("chopstick4") == "Socrates" and\
+                        self.get_value("chopstick5") == "Socrates":
+                    # chopsticks are released
+                    self.emit_signal("chopstick4", "")
+                    self.emit_signal("chopstick5", "")
         if name == "Kant":
             if data[1] == "HUNGRY":
-                if not self.get_value("fork5") and not self.get_value("fork1"):
-                    # forks are available
-                    self.emit_signal("fork5", "Kant")
-                    self.emit_signal("fork1", "Kant")
+                if not self.get_value("chopstick5") and not self.get_value("chopstick1"):
+                    # chopsticks are available
+                    self.emit_signal("chopstick5", "Kant")
+                    self.emit_signal("chopstick1", "Kant")
             if data[1] == "THINKING":
-                if self.get_value("fork5") == "Kant" and\
-                        self.get_value("fork1") == "Kant":
-                    # forks are released
-                    self.emit_signal("fork5", "")
-                    self.emit_signal("fork1", "")
+                if self.get_value("chopstick5") == "Kant" and\
+                        self.get_value("chopstick1") == "Kant":
+                    # chopsticks are released
+                    self.emit_signal("chopstick5", "")
+                    self.emit_signal("chopstick1", "")
 
     def on_peer_exit(self, peer, name, *args, **kwargs):
         print("EXIT", peer, name)
@@ -155,35 +155,35 @@ class Waiter(LeadActor):
     def update(self):
         # thread 1
         if self.get_value("state1") == "HUNGRY":
-            if not self.get_value("fork1"):
-                self.emit_signal("fork1", "Descartes")
-            if self.get_value("fork1") == "Descartes" and not self.get_value("fork2"):
-                self.emit_signal("fork2", "Descartes")
+            if not self.get_value("chopstick1"):
+                self.emit_signal("chopstick1", "Descartes")
+            if self.get_value("chopstick1") == "Descartes" and not self.get_value("chopstick2"):
+                self.emit_signal("chopstick2", "Descartes")
 
         # thread 2
         if self.get_value("state2") == "HUNGRY":
-            if not self.get_value("fork2"):
-                self.emit_signal("fork2", "Plato")
-            if self.get_value("fork2") == "Plato" and not self.get_value("fork3"):
-                self.emit_signal("fork3", "Plato")
+            if not self.get_value("chopstick2"):
+                self.emit_signal("chopstick2", "Plato")
+            if self.get_value("chopstick2") == "Plato" and not self.get_value("chopstick3"):
+                self.emit_signal("chopstick3", "Plato")
         # thread 3
         if self.get_value("state3") == "HUNGRY":
-            if not self.get_value("fork3"):
-                self.emit_signal("fork3", "Aristotle")
-            if self.get_value("fork3") == "Aristotle" and not self.get_value("fork4"):
-                self.emit_signal("fork4", "Aristotle")
+            if not self.get_value("chopstick3"):
+                self.emit_signal("chopstick3", "Aristotle")
+            if self.get_value("chopstick3") == "Aristotle" and not self.get_value("chopstick4"):
+                self.emit_signal("chopstick4", "Aristotle")
         # thread 4
         if self.get_value("state4") == "HUNGRY":
-            if not self.get_value("fork4"):
-                self.emit_signal("fork4", "Socrates")
-            if self.get_value("fork4") == "Socrates" and not self.get_value("fork5"):
-                self.emit_signal("fork5", "Socrates")
+            if not self.get_value("chopstick4"):
+                self.emit_signal("chopstick4", "Socrates")
+            if self.get_value("chopstick4") == "Socrates" and not self.get_value("chopstick5"):
+                self.emit_signal("chopstick5", "Socrates")
         # thread 5
         if self.get_value("state5") == "HUNGRY":
-            if not self.get_value("fork5"):
-                self.emit_signal("fork5", "Kant")
-            if self.get_value("fork5") == "Kant" and not self.get_value("fork1"):
-                self.emit_signal("fork1", "Kant")
+            if not self.get_value("chopstick5"):
+                self.emit_signal("chopstick5", "Kant")
+            if self.get_value("chopstick5") == "Kant" and not self.get_value("chopstick1"):
+                self.emit_signal("chopstick1", "Kant")
 
 
 if __name__ == "__main__":
