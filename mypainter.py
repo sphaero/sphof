71de49f6f2174ea8ae0bc8c6c2230b3fddb98133
Arnaud Loonstra <arnaud@sphaero.org>
2015-06-24 08:46:46 +0200

    painter example cleanup

--- mypainter.py
+++ mypainter.py
@@ -6,18 +6,14 @@ class MyPainter(PainterActor):
 
     def setup(self):
         self.count = 0
-        return
 
     def update(self):
         self.count += 1
         if self.count > 60 and len(sphof.shared_ns) < 8:
             self.send_img()
             self.count = 0
-            #print(self.count)
 
     def draw(self):
-        #self._img = Image.new("RGB", (200,600), (255,255,55))
-        #self._d = ImageDraw.Draw(self._img)
         start = (randint(0,200), randint(0,600))
         end = (randint(0,200), randint(0,600))
         color = (randint(70,110), randint(160,210), randint(70,210))
